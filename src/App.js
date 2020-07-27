1e18a2a97578237e391daa724bdb47badbbf66d1
rosebutch <annathedm@gmail.com>
2020-07-27 14:31:50 -0500

    added overlay

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <h1>mystic mirror</h1>
+        <p> use the buttons below to generate an aura reading </p>
         <MirrorCam />
       </div>
     )
--- src/components/MirrorCam.js
+++ src/components/MirrorCam.js
@@ -1,19 +1,36 @@
 import React, {useState} from "react"
 import Webcam from 'react-webcam'
+import "./Filter.css"
 
 const MirrorCam = () => {
   const [camera, setCamera] = useState(true)
+  const [overlayStyle, setOverlayStyle] = useState({})
 
   const toggleCamera = () => {
+    setOverlayStyle({})
     setCamera(!camera)
+  }
+
+  const changeColor = () => {
+    setOverlayStyle({
+      backgroundImage: `radial-gradient(#${generateRandomColor()}, #${generateRandomColor()}, #${generateRandomColor()})`
+    })
+  }
+
+  const generateRandomColor = () => {
+    return Math.floor(Math.random()*16777215).toString(16)
   }
 
   return (
     <div>
-      <div className='card' >
+      <div className='card camera' >
+        {camera && overlayStyle && <div className='overlay' style={overlayStyle} />}
         {camera && <Webcam mirrored={true} audio={false}/>}
       </div>
-      <button class="btn btn-info" onClick={toggleCamera}>{camera? "off":"on" }</button>
+      <div className='alert'>
+        <button className="btn btn-info m-3" onClick={toggleCamera}>{camera? "camera off":"camera on" }</button>
+        {camera && <button className="btn btn-info m-3" onClick={changeColor}>generate reading</button>}
+      </div>
     </div>
   )
 }
