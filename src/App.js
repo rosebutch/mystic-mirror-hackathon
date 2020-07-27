555d15bf225749c3f7514c1df76eb5375e3d1c45
rosebutch <annathedm@gmail.com>
2020-07-27 12:24:58 -0500

    added webcam!

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import React, { Component } from "react"
-import logo from "./logo.svg"
 import "./App.css"
+import Webcam from 'react-webcam'
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-        </header>
+        <Webcam />
       </div>
     )
   }
